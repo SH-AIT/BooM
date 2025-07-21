42db1fbd6f66f863e7eff020d0a1e5fce0d2a6d6
lijiaming <1228575330@qq.com>
2025-07-21 09:45:46 +0800

    部署加速

--- script/mindspore-deepseek/workspace/roles/prepare/files/lib/ray_start.sh
+++ script/mindspore-deepseek/workspace/roles/prepare/files/lib/ray_start.sh
@@ -6,7 +6,9 @@ current_path=$(
 ENV_FILE=/root/.bashrc
 source $current_path/config.cfg
 source $ENV_FILE
+
 ray_start() {
+
     ps -ef | grep "python" | grep -v grep | awk '{print $2}' | xargs kill
     if [ $NODE_NUM -eq 1 ]; then
         echo "单机部署无需启动ray"
@@ -17,6 +19,28 @@ ray_start() {
     if [ "$1" ]; then
         # 从节点
         nohup ray start --address=$1:$RAY_PORT &
+        sleep 5
+        if [ $NODE_NUM -eq 2 ]; then
+            NPU_NUM=16.0
+        elif [ $NODE_NUM -eq 4 ]; then
+            NPU_NUM=32.0
+        fi
+
+        ray_status=0
+        for i in {1..10}; do
+            ray status | grep "$NPU_NUM NPU"
+            if [ $? -eq 0 ]; then
+                echo "ray集群已全部拉起"
+                ray_status=1
+                break
+            fi
+            sleep 3
+        done
+
+        if [ $ray_status -eq 0 ]; then
+            echo "ray集群超时"
+            exit 1
+        fi
     else
         # 主节点
         nohup ray start --head --include-dashboard=False --port=$RAY_PORT &
